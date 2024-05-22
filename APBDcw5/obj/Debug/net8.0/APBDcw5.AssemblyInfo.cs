b72efaf23d6393187d0379fa04029b00769ae9b3
Kacper <s25972@pjwstk.edu.pl>
2024-05-22 23:43:37 +0200

    Działa

--- APBDcw5/obj/Debug/net8.0/APBDcw5.AssemblyInfo.cs
+++ APBDcw5/obj/Debug/net8.0/APBDcw5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBDcw5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d388f0299e04fe5835a487c568015c97008db8b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBDcw5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBDcw5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
